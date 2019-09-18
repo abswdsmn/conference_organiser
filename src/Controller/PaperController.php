695fc1dcb6548a44e808fe76ace3e046c6ca99a1
abswdsmn <mark.c.maynard@gmail.com>
2019-09-18 17:01:01 +0100

    - added a new entity 'Paper' with a file upload.

---
+++ src/Controller/PaperController.php
@@ -0,0 +1,58 @@
+<?php
+// src/Controller/PaperController.php
+
+namespace App\Controller;
+
+use App\Entity\Paper;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+/**
+ * Class PaperController
+ *
+ * Deals with managing papers.
+ *
+ * @package App\Controller
+ */
+class PaperController extends AbstractController
+{
+    /**
+     * Make a new paper.
+     *
+     * @Route("/paper", name="paper")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function createPaper(Request $request) : Response
+    {
+        $paper = new Paper();
+        $form = $this->createFormBuilder($paper)
+            ->add('paperFile', VichImageType::class, ['label' => 'Upload file here'])
+            ->add('save', SubmitType::class, ['label' => 'Save paper'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($paper);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('user.html.twig', ['form' => $form->createView()]);
+    }
+}
--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -3,18 +3,15 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
